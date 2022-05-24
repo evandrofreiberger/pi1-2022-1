44b984880144c9568a3aab676f3c5b55d40f0fb6
MatheusSantiagos <matheus.ssantiag@gmail.com>
2022-05-23 20:10:34 -0400

    Adicionando EntregaVO.java

---
+++ src/main/java/ifmt/cba/pi1/vo/CursoVO.java
@@ -0,0 +1,36 @@
+package ifmt.cba.pi1.vo;
+
+public class CursoVO {
+    private int codigo;
+    private String nome;
+
+
+
+public CursoVO(){
+    this.codigo = 0;
+    this.nome = " ";
+}
+
+public CursoVO(int codigo, String nome){
+    this();
+    this.codigo = codigo;
+    this.nome = nome;
+}
+
+    public int getCodigo(){
+        return codigo;
+    }
+
+    public void setcodigo (int codigo){
+        this.codigo = codigo;
+    }
+
+    public String getNome (){
+        return nome;
+    }
+
+    public void setNome (String Nome){
+        this.nome = nome;
+    }
+
+}
