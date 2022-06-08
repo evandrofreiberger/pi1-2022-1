7ddaacbb98e97edf4bcbc35acb7c83c922de9733
Ricardosantos31 <riacrdok9@gmail.com>
2022-06-08 12:45:01 -0400

    Adiciona `AlunoDAO`

---
+++ src/main/java/ifmt/cba/pi1/persistencia/AlunoDAO.java
@@ -0,0 +1,57 @@
+package ifmt.cba.pi1.persistencia;
+
+import ifmt.cba.pi1.vo.AlunoVO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class AlunoDAO extends DAO<AlunoVO> implements IAlunoDAO {
+
+
+    public AlunoDAO(EntityManager entityManager) throws PersistenciaException {
+        super(entityManager);
+    }
+
+    @Override
+    public AlunoVO buscarPorMatricula(int matricula) throws PersistenciaException {
+        AlunoVO alunoVO = null;
+
+        try {
+            alunoVO = this.entityManager.find(AlunoVO.class, matricula);
+        } catch (Exception ex) {
+            throw new PersistenciaException("Erro na selecao por matricula");
+        }
+        return alunoVO;
+    }
+
+    @Override
+    public List<AlunoVO> buscarPorNome(String nome) throws PersistenciaException {
+        List<AlunoVO> listaAlunos = new ArrayList<>();
+        try {
+            Query query = this.entityManager.createQuery("SELECT a FROM AlunoVO a WHERE UPPER(a.nome) LIKE :pNome ORDER BY a.nome");
+            query.setParameter("pNome", "%" + nome.toUpperCase().trim() + "%");
+            listaAlunos = query.getResultList();
+        } catch (Exception ex) {
+            throw new PersistenciaException("Erro na selecao por nome");
+        }
+        return listaAlunos;
+    }
+
+    @Override
+    public void beginTransaction() {
+        this.entityManager.getTransaction().begin();
+    }
+
+    @Override
+    public void commitTransaction() {
+        this.entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public void rollbackTransaction() {
+        this.entityManager.getTransaction().rollback();
+    }
+}
