0c156f0fc11337c5048cf37c3ccb37e2902ab39f
Ricardosantos31 <riacrdok9@gmail.com>
2022-06-08 12:46:17 -0400

    Adiciona instancia de `AlunoDAO` em `AlunoNegocio`

--- src/main/java/ifmt/cba/pi1/negocio/AlunoNegocio.java
+++ src/main/java/ifmt/cba/pi1/negocio/AlunoNegocio.java
@@ -2,103 +2,109 @@ package ifmt.cba.pi1.negocio;
 
 import java.util.List;
 
+import ifmt.cba.pi1.persistencia.AlunoDAO;
+import ifmt.cba.pi1.persistencia.FabricaEntityManager;
 import ifmt.cba.pi1.persistencia.IAlunoDAO;
 import ifmt.cba.pi1.persistencia.PersistenciaException;
 import ifmt.cba.pi1.vo.AlunoVO;
 
 public class AlunoNegocio {
 
-	private IAlunoDAO alunoDAO;
+    private AlunoDAO alunoDAO;
 
-	public AlunoNegocio() throws NegocioException {
-		// aqui sera instanciado um objeto AlunoDAO
-	}
+    public AlunoNegocio() throws NegocioException {
+        try {
+            this.alunoDAO = new AlunoDAO(FabricaEntityManager.getEntityManager());
+        } catch (PersistenciaException e) {
+            throw new NegocioException("Erro ao iniciar a persistencia -- " + e.getMessage());
+        }
+    }
 
-	public void inserir(AlunoVO alunoVO) throws NegocioException {
+    public void inserir(AlunoVO alunoVO) throws NegocioException {
 
-		String mensagemErros = this.validarDados(alunoVO);
+        String mensagemErros = this.validarDados(alunoVO);
 
-		if (!mensagemErros.isEmpty()) {
-			throw new NegocioException(mensagemErros);
-		}
+        if (!mensagemErros.isEmpty()) {
+            throw new NegocioException(mensagemErros);
+        }
 
-		if (this.alunoDAO != null) {
-			try {
-				alunoDAO.beginTransaction();
-				alunoDAO.incluir(alunoVO);
-				alunoDAO.commitTransaction();
-			} catch (PersistenciaException ex) {
-				alunoDAO.rollbackTransaction();
-				throw new NegocioException("Erro ao incluir o aluno - " + ex.getMessage());
-			}
-		}
-	}
+        if (this.alunoDAO != null) {
+            try {
+                alunoDAO.beginTransaction();
+                alunoDAO.incluir(alunoVO);
+                alunoDAO.commitTransaction();
+            } catch (PersistenciaException ex) {
+                alunoDAO.rollbackTransaction();
+                throw new NegocioException("Erro ao incluir o aluno - " + ex.getMessage());
+            }
+        }
+    }
 
-	public void alterar(AlunoVO alunoVO) throws NegocioException {
-		String mensagemErros = this.validarDados(alunoVO);
-		if (!mensagemErros.isEmpty()) {
-			throw new NegocioException(mensagemErros);
-		}
+    public void alterar(AlunoVO alunoVO) throws NegocioException {
+        String mensagemErros = this.validarDados(alunoVO);
+        if (!mensagemErros.isEmpty()) {
+            throw new NegocioException(mensagemErros);
+        }
 
-		if (this.alunoDAO != null) {
-			try {
-				alunoDAO.beginTransaction();
-				alunoDAO.alterar(alunoVO);
-				alunoDAO.commitTransaction();
-			} catch (PersistenciaException ex) {
-				alunoDAO.rollbackTransaction();
-				throw new NegocioException("Erro ao alterar o aluno - " + ex.getMessage());
-			}
-		}
-	}
+        if (this.alunoDAO != null) {
+            try {
+                alunoDAO.beginTransaction();
+                alunoDAO.alterar(alunoVO);
+                alunoDAO.commitTransaction();
+            } catch (PersistenciaException ex) {
+                alunoDAO.rollbackTransaction();
+                throw new NegocioException("Erro ao alterar o aluno - " + ex.getMessage());
+            }
+        }
+    }
 
-	public void excluir(AlunoVO alunoVO) throws NegocioException {
-		if (this.alunoDAO != null) {
-			try {
-				alunoDAO.beginTransaction();
-				alunoDAO.excluir(alunoVO);
-				alunoDAO.commitTransaction();
-			} catch (PersistenciaException ex) {
-				alunoDAO.rollbackTransaction();
-				throw new NegocioException("Erro ao excluir o aluno - " + ex.getMessage());
-			}
-		}
-	}
+    public void excluir(AlunoVO alunoVO) throws NegocioException {
+        if (this.alunoDAO != null) {
+            try {
+                alunoDAO.beginTransaction();
+                alunoDAO.excluir(alunoVO);
+                alunoDAO.commitTransaction();
+            } catch (PersistenciaException ex) {
+                alunoDAO.rollbackTransaction();
+                throw new NegocioException("Erro ao excluir o aluno - " + ex.getMessage());
+            }
+        }
+    }
 
-	public List<AlunoVO> pesquisaParteNome(String parteNome) throws NegocioException {
-		if (this.alunoDAO != null) {
-			try {
-				return alunoDAO.buscarPorNome(parteNome);
-			} catch (PersistenciaException ex) {
-				throw new NegocioException("Erro ao pesquisar aluno pelo nome - " + ex.getMessage());
-			}
-		} 
-		return null;
-	}
+    public List<AlunoVO> pesquisaPorNome(String parteNome) throws NegocioException {
+        if (this.alunoDAO != null) {
+            try {
+                return alunoDAO.buscarPorNome(parteNome);
+            } catch (PersistenciaException ex) {
+                throw new NegocioException("Erro ao pesquisar aluno pelo nome - " + ex.getMessage());
+            }
+        }
+        return null;
+    }
 
-	public AlunoVO pesquisaMatricula(int matricula) throws NegocioException {
-		if (this.alunoDAO != null) {
-			try {
-				return alunoDAO.buscarPorMatricula(matricula);
-			} catch (PersistenciaException ex) {
-				throw new NegocioException("Erro ao pesquisar aluno pela matricula - " + ex.getMessage());
-			}
-		}
-		return null;
-	}
+    public AlunoVO pesquisaMatricula(int matricula) throws NegocioException {
+        if (this.alunoDAO != null) {
+            try {
+                return alunoDAO.buscarPorMatricula(matricula);
+            } catch (PersistenciaException ex) {
+                throw new NegocioException("Erro ao pesquisar aluno pela matricula - " + ex.getMessage());
+            }
+        }
+        return null;
+    }
 
-	private String validarDados(AlunoVO alunoVO) {
+    private String validarDados(AlunoVO alunoVO) {
 
-		String mensagemErros = "";
+        String mensagemErros = "";
 
-		if (alunoVO.getMatricula() <= 0) {
-			mensagemErros += "Matricula deve ser maior que zero";
-		}
+        if (alunoVO.getMatricula() < 0) {
+            mensagemErros += "Matricula deve ser maior que zero";
+        }
 
-		if (alunoVO.getNome() == null || alunoVO.getNome().length() == 0) {
-			mensagemErros += "Nome do aluno nao pode ser vazio";
-		}
+        if (alunoVO.getNome() == null || alunoVO.getNome().length() == 0) {
+            mensagemErros += "Nome do aluno nao pode ser vazio";
+        }
 
-		return mensagemErros;
-	}
+        return mensagemErros;
+    }
 }
