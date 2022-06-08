59b799f3a76c62ff7927dfcd2f69901329148168
Ricardosantos31 <riacrdok9@gmail.com>
2022-06-08 12:44:17 -0400

    Adiciona classe base do DAO

---
+++ src/main/java/ifmt/cba/pi1/persistencia/DAO.java
@@ -0,0 +1,39 @@
+package ifmt.cba.pi1.persistencia;
+
+import ifmt.cba.pi1.vo.AlunoVO;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public abstract class DAO<VO> {
+
+    protected EntityManager entityManager;
+
+    public DAO(EntityManager entityManager) throws PersistenciaException {
+        this.entityManager = entityManager;
+    }
+
+    public void incluir(VO vo) throws PersistenciaException {
+        try {
+            this.entityManager.persist(vo);
+        } catch (Exception e) {
+            throw new PersistenciaException("Erro ao incluir " + vo.getClass() + " - " + e.getMessage());
+        }
+    }
+
+    public void alterar(VO vo) throws PersistenciaException {
+        try {
+            this.entityManager.merge(vo);
+        } catch (Exception e) {
+            throw new PersistenciaException("Erro ao alterar " + vo.getClass() + " - " + e.getMessage());
+        }
+    }
+
+    public void excluir(VO vo) throws PersistenciaException {
+        try {
+            this.entityManager.remove(vo);
+        } catch (Exception e) {
+            throw new PersistenciaException("Erro ao excluir " + vo.getClass() + " - " + e.getMessage());
+        }
+    }
+}
